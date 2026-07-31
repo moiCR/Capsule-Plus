37a2c33f1b01aeefa2c611667e1f10cce8bd3f09
Moisés <128289039+moiCR@users.noreply.github.com>
2026-07-31 16:33:26 -0600

    Fix command case for capsule in autostart.lua

--- hypr/modules/autostart.lua
+++ hypr/modules/autostart.lua
@@ -1,7 +1,8 @@
 local programs = require("modules/programs")
 
 hl.on("hyprland.start", function ()
+    hl.exec_cmd("awww-daemon")
     hl.exec_cmd("dbus-update-activation-environment --systemd --all")
-    hl.exec_cmd("Capsule")
+    hl.exec_cmd("capsule")
     hl.exec_cmd("xhost +SI:localuser:root")
 end)
